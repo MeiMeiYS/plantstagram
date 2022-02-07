fcf4a282ea8df90c6c1f20cf0293044ccae0012a
Mei Yin Shih <meiyinshih@Meis-MBP.attlocal.net>
2022-02-07 08:25:25 -0800

    Move logout button to home page for now; Added navbar searchBar with no functionality yet; Added navbar button group, the left for links are working, will continue working on the last 2 button's functionality:

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,6 +8,7 @@ import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
+import LogoutButton from './components/auth/LogoutButton';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -42,6 +43,7 @@ function App() {
         </ProtectedRoute>
         <ProtectedRoute path='/' exact={true} >
           <h1>My Home Page</h1>
+          <LogoutButton />
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
