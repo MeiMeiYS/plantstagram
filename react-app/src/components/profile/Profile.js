79ff0608c0b14c608557b7c1fd2430d3d2b5f24d
App Academy <sure777.shz@gmail.com>
2022-02-10 08:17:11 -0800

    nothing

--- react-app/src/components/profile/Profile.js
+++ react-app/src/components/profile/Profile.js
@@ -13,7 +13,7 @@ const Profile = () => {
     const settingBtn = useRef();
 
     const handleFollowers = (e) => {
-       return <FollowersModal />
+    //    return <FollowersModal />
     }
     useEffect(() => {
         if (!userId) {
