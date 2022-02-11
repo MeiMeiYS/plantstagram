5d542d12602440c9bf7d4e84f2691667665d6eee
App Academy <sure777.shz@gmail.com>
2022-02-10 16:56:45 -0800

    css for following_followers

--- react-app/src/components/profile/FollowModal.js
+++ react-app/src/components/profile/FollowModal.js
@@ -9,6 +9,7 @@ const FollowersModal = ({userId, setShowFollowing, showFollowing, setShowFollowe
     const [allFollowers, setAllFollowers] = useState({});
     const [allFollowings, setAllFollowings] = useState({});
     const [list, setList] = useState([])
+    const [header, setHeader] = useState("")
     useEffect(()=> {
         if (showFollowers && overlay) dispatch(getFollowers(userId)).then(res => setAllFollowers(res));
     }, [overlay])
@@ -24,6 +25,7 @@ const FollowersModal = ({userId, setShowFollowing, showFollowing, setShowFollowe
             values.forEach(follower => tempList.push(follower));
             console.log(allFollowers)
             setList(tempList)
+            setHeader("Follower")
         }
 
     }, [allFollowers])
@@ -35,6 +37,7 @@ const FollowersModal = ({userId, setShowFollowing, showFollowing, setShowFollowe
             values.forEach(following => tempList1.push(following));
             console.log(allFollowings)
             setList(tempList1)
+            setHeader("Following")
         }
     }, [allFollowings])
 
@@ -48,15 +51,20 @@ const FollowersModal = ({userId, setShowFollowing, showFollowing, setShowFollowe
     return (
         <div className="background-overlay" onClick={handleOverlay}>
                 <div className="follow-modal">
-                    <div className="header">
+                    <div className="header">{header}
                     </div>
                     <div className="main-content">
                         {list.length && list.map(userObj => (
                             <div className='follow-info-container' key={userObj.username}>
-                                <div className='follow-pic'>{userObj.avatar_url}</div>
+                                <img className="follower_profile_pic" src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Question-mark-face.jpg" alt="Profile Image" />
+                                {/* <div className='follow-pic'>{userObj.avatar_url}</div> */}
+                                <div className="username_name">
                                 <div className='follow-username'>{userObj.username}</div>
                                 <div className='follow-name'>{userObj.name}</div>
-                                <button>Remove</button>
+                                </div>
+                                <div className="rv_btn_container">
+                                    <button className="remove_followers">Remove</button>
+                                </div>
                             </div>
                         ))}
                     </div>
